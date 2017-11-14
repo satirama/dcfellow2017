244b51ecf4ac054a3815ca408db5828e88aaa80d
unknown <mirandas.angelica@gmail.com>
2017-11-14 13:21:58 -0600

    server side ready

--- noviembre/08/server/routes/books.js
+++ noviembre/08/server/routes/books.js
@@ -3,48 +3,53 @@ var router = express.Router();
 var csv = require('csvtojson');
 var cors = require('cors');
 
- 
 /* GET home page. */
 /* Read all */
-router.get('/', function(req, res, next) {
+router.get('/', function(req, res) {
   csv().fromFile('./books.csv')
   .on('json', (json) =>{
-    json
   })
   .on('end_parsed', (jArr) =>{
     res.json({status:200, books: jArr});
   })
   .on('error',(error) =>{
-    res.send({status: 500})
+    res.send({status: 500});
   })
-});
+})
+
+// Get authors
+router.get('/authors', function (req, res) {
+  var aArr = [];
+  csv({
+    includeColumns: [7]
+  })
+  .fromFile('./books.csv')
+  .on('json', (jObj) => {
+    if (aArr.indexOf(jObj.authors) < 0){
+      aArr.push(jObj.authors);
+    }
+  })
+  .on('end', () => {
+    res.json({status:200, authorsArr: aArr});
+  })
+  .on('error', (error) =>{
+    res.send({status: 500});
+  })
+})
+
 
 /* Read one */
-router.get('/:id', function(req, res, next){
-    console.log(req.params);
-    var response = {status: 200, book : []};
-    csv().fromFile('./books.csv')
-    .on('json', (jObj) =>{
-        jObj
-      /*if (jObj.id == req.params){
-          console.log(jObj)
-          //res.json({status: 200, book: c})
-          //response.book.push(json)
-      }*/
-    })
-    .on('end_parsed',(jArrObj) =>{
-        for (var i = 0; i < jArrObj.lenght; i++){
-            console.log(jArrObj[i].id);
-            if (jArrObj[i].id == req.params){
-                response.book.push(jArrObj[i]);
-                console.log('checked')
-            }
-        }
-        res.json(response)
-    })
-    .on('error',(error) =>{
-      res.send({status: 500})
-    })
+router.get('/:id', function(req, res){
+  csv().fromFile('./books.csv')
+  .on('json', (jObj) =>{
+    if (jObj.id === req.params.id){
+      res.json({status: 200, book : jObj});
+    }
+  })
+  .on('error',(error) =>{
+    console.log('error');
+    res.send({status: 500});
+  })
 })
 
 module.exports = router;
